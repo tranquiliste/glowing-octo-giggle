f9b1c4d23153be380e3bcb6967bc210efafabef8
Nicolas Aguttes <nicolas.aguttes@gmail..com>
2017-07-05 21:46:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony
+=======
+
+A Symfony project created on June 28, 2017, 8:07 pm.
---
+++ src/OC/PlatformBundle/Tests/Controller/AdvertControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OC\PlatformBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdvertControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/index');
+    }
+
+}

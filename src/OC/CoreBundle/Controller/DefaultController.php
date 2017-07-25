7ff197b8c76ad5e42dfc3d7e58f2d6713b1739e6
Nicolas Aguttes <nicolas.aguttes@gmail..com>
2017-07-25 07:39:04 +0200

    exercice contact form

--- src/OC/CoreBundle/Controller/DefaultController.php
+++ src/OC/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace OC\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('OCCoreBundle:Default:index.html.twig');
+    }
+    public function contactAction( Request $request)
+    {
+    	// récupération de la session
+ 		$session = $request->getSession();
+        $session->getFlashBag()->add('info', 'Désolé je ne sais pas faire de formulaire ;)');
+
+        return $this->redirectToRoute('oc_core_homepage');
     }
 }
